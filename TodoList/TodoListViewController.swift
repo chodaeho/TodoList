26f70e09e8ccd186ea42e2edad3cad4bdbcf76b7
daehocho <daehocho0637@gmail.com>
2021-12-18 22:45:53 +0900

    TodoListViewController 로직 구현

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -19,7 +19,7 @@ class TodoListViewController: UIViewController {
     
     
     // TODO: TodoViewModel 만들기
-    
+    let todoListViewModel = TodoViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +28,7 @@ class TodoListViewController: UIViewController {
         
         
         // TODO: 데이터 불러오기
-        
+        todoListViewModel.loadTasks()
     }
     
     @IBAction func isTodayButtonTapped(_ sender: Any) {
@@ -56,24 +56,38 @@ extension TodoListViewController {
 extension TodoListViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         // TODO: 섹션 몇개
-        return 2
+        return todoListViewModel.numOfSection
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // TODO: 섹션별 아이템 몇개
-        return 10
+        if section == 0 {
+            return todoListViewModel.todayTodos.count
+        } else {
+            return todoListViewModel.upcompingTodos.count
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         // TODO: 커스텀 셀
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TodoListCell", for: indexPath) as? TodoListCell else {
             return UICollectionViewCell()
         }
-        return cell
         
         // TODO: todo 를 이용해서 updateUI
+        var todo: Todo
+        if indexPath.section == 0 {
+            todo = todoListViewModel.todayTodos[indexPath.item]
+        } else {
+            todo = todoListViewModel.upcompingTodos[indexPath.item]
+        }
+        cell.updateUI(todo: todo)
+        
         // TODO: doneButtonHandler 작성
         // TODO: deleteButtonHandler 작성
+        
         return cell
     }
     
